0338ffdbb03331ecf213afebef6e61d0914bf4f2
guidari <guilhermedatilio@gmail.com>
2023-08-04 12:25:15 -0300

    feat(tutorial): complete step 1

---
+++ src/app/providers.js
@@ -0,0 +1,18 @@
+"use client";
+
+import { Content, Theme } from "@carbon/react";
+
+import TutorialHeader from "@/components/TutorialHeader/TutorialHeader";
+
+const Providers = ({ children }) => {
+  return (
+    <div>
+      <Theme theme="g100">
+        <TutorialHeader />
+      </Theme>
+      <Content>{children}</Content>
+    </div>
+  );
+};
+
+export default Providers;
